b84784728e2e6a0cea500b0d760782557a1694f9
nrmarston <1916148+nrmarston@users.noreply.github.com>
2021-05-18 22:52:44 -0500

    Update postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -2,7 +2,7 @@
 
 // Helper library written for useful postprocessing tasks with Flat Data
 // Has helper functions for manipulating csv, json, excel, zip, and image files
-import { readJSON, writeJSON, removeFile } from 'https://deno.land/x/flat@0.0.9/mod.ts' 
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.9/mod.ts' 
 
 // Step 1: Read the downloaded_filename JSON
 const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
@@ -18,6 +18,5 @@ const filteredCurrencyRates = currencyRates.map(rate => ({
 
 // Step 3. Write a new JSON file with our filtered data
 const newFilename = `btc-price-postprocessed.json` // name of a new file to be saved
-await removeFile(filename) // or await removeFile(‘btc-price.json’)
 await writeJSON(newFilename, filteredCurrencyRates) // create a new JSON file with just the Bitcoin price
 console.log("Wrote a post process file")
